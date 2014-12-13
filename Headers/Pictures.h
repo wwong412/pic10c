a48e850a775a1a5214dc471e6b906cfd745c1f08
wwong412 <wwong412@gmail.com>
2014-12-12 16:01:09 -0800

    Create Pictures.h

---
+++ Headers/Pictures.h
@@ -0,0 +1,92 @@
+#ifndef PICTURES_H
+#define PICTURES_H
+
+#include <QPixmap>
+
+// class loads all photos
+class Pictures
+{
+public:
+    Pictures();
+    void load();
+
+    QPixmap* heart_pic;
+    QPixmap* coin_pic;
+
+    QPixmap* vertical ;
+    QPixmap* across ;
+    QPixmap* rd3 ;
+    QPixmap* lu4 ;
+    QPixmap* ur5 ;
+    QPixmap* dr6;
+
+    QPixmap* tree1;
+
+    QPixmap* squirrel;
+    QPixmap* squirrel2;
+    QPixmap* squirrel3;
+
+    QPixmap* porcupine;
+    QPixmap* porcupine2;
+    QPixmap* porcupine3;
+
+    QPixmap* spider ;
+    QPixmap* spider2 ;
+    QPixmap* spider3;
+
+    QPixmap* frog;
+    QPixmap* frog2;
+    QPixmap* frog3;
+    QPixmap* frog4;
+
+    QPixmap* sPick;
+    QPixmap* pPick;
+    QPixmap* spidPick;
+    QPixmap* fPick;
+    QPixmap* shine;
+
+    QPixmap* sell;
+    QPixmap* upgrade;
+    QPixmap* cancel;
+
+    QPixmap* start;
+
+    QPixmap* man;
+    QPixmap* man2;
+    QPixmap* man3;
+    QPixmap* man4;
+
+    QPixmap* hatman;
+    QPixmap* hatman2;
+    QPixmap* hatman3;
+    QPixmap* hatman4;
+
+    QPixmap* jackman1;
+    QPixmap* jackman2;
+
+    QPixmap* dozer;
+    QPixmap* dozer2;
+    QPixmap* dozer3;
+
+    QPixmap* range;
+
+    QPixmap* acorn;
+    QPixmap* quill;
+    QPixmap* web;
+
+    QPixmap* pause;
+
+    QPixmap* mTree;
+    QPixmap* stump;
+
+    QPixmap* speed1;
+    QPixmap* speed2;
+    QPixmap* speed3;
+
+    QPixmap* resumeButton;
+    QPixmap* quitButton;
+    QPixmap* restartButton;
+
+};
+
+#endif // PICTURES_H
